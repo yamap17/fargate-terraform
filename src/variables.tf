344db479b0f8a19d6985e587ce005d990cc8daa2
Yuya Yamada <yuya_0602@icloud.com>
2023-01-22 23:54:03 +0900

    add s3 and network

--- src/variables.tf
+++ src/variables.tf
@@ -1,5 +1,15 @@
-variable aws_access_key {}
-variable aws_secret_key {}
-variable aws_region {}
-variable aws_vpc_cidr {}
-variable aws_tag_name {}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
+variable "aws_account_id" {}
+variable "alb_log_s3_bucket_name" {
+  type        = string
+  description = "alb log s3 bucket name"
+}
+variable "vpc_cidr" {}
+variable "subnet_public_a_cidr" {}
+variable "subnet_public_c_cidr" {}
+variable "subnet_private_a_cidr" {}
+variable "subnet_private_c_cidr" {}
+variable "access_ingress_cidr" {}
+variable "service_tag_name" {}
